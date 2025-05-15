74687ba6275a993a58dd9d52d64160c41108a984
佐渡隆之介 <ryunosukesado@net90-dhcp-173.sfc.keio.ac.jp>
2025-05-15 16:05:18 +0900

    Add Vercel Analytics to project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
 	injectSpeedInsights();
+	injectAnalytics();
 </script>
 
 <slot />
