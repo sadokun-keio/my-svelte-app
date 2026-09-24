48914a68f757e52e550985f2a0ed5851df4a22c0
sadokun-keio <ryunosukesado@keio.jp>
2026-09-25 04:53:00 +0900

    feat: add Tailwind CSS v4 and Skeleton UI (cerberus theme)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import '../app.css';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import { page } from '$app/stores';
